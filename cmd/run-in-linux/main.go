d39a73cdbd0006f9ce864e554a4ea55836cbd65d
pefish <pefish@qq.com>
2020-08-17 15:42:59 +0800

    Fix bug

--- cmd/run-in-linux/main.go
+++ cmd/run-in-linux/main.go
@@ -3,28 +3,34 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/pefish/run-in-linux/shell"
+	"github.com/pefish/run-in-linux/pkg/shell"
+	"github.com/pefish/run-in-linux/version"
 	"log"
 	"os"
 	"strings"
 )
 
-// docker run -t -i -v `pwd`:/app -w /app -e GOPROXY=https://goproxy.io golang:1.14 go run ./bin/test
+// docker run -t -i -v `pwd`:/app -w /app -e GOPROXY=https://goproxy.io golang:1.14 go run ./cmd/test
 
 func main() {
-	flagSet := flag.NewFlagSet("run-in-linux", flag.ExitOnError)
+	flagSet := flag.NewFlagSet(version.Name, flag.ExitOnError)
 	env := flagSet.String("e", "GOPROXY=https://goproxy.io", "环境变量，可以多次指定。例如 GOPROXY=https://goproxy.io,ABC=abc")
 	port := flagSet.String("p", "", "端口映射。例如 3333:80,3334=81")
 	image := flagSet.String("i", "golang:1.14", "镜像名")
+	printVersion := flagSet.Bool("v", false, "打印版本号")
 	flagSet.Usage = func() {
-		fmt.Fprintf(flagSet.Output(), "\n%s%s 是一个切换环境执行命令的工具\n\n", strings.ToUpper(string(flagSet.Name()[0])), flagSet.Name()[1:])
-		fmt.Fprintf(flagSet.Output(), "Usage of %s:\n", flagSet.Name())
+		fmt.Printf("\n%s%s 是一个切换环境执行命令的工具\n\n", strings.ToUpper(string(flagSet.Name()[0])), flagSet.Name()[1:])
+		fmt.Printf("Usage of %s:\n", flagSet.Name())
 		flagSet.PrintDefaults()
-		fmt.Fprintf(flagSet.Output(), "\n")
+		fmt.Printf("\n")
 	}
 	err := flagSet.Parse(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
+	}
+	if *printVersion {
+		fmt.Println(version.Version)
+		os.Exit(0)
 	}
 	args := flagSet.Args()
 	if len(args) == 0 {
@@ -36,7 +42,7 @@ func main() {
 	for _, e := range strings.Split(*env, ",") {
 		cmdArr = append(cmdArr, "-e", e)
 	}
-	if port != nil {
+	if port != nil && *port != "" {
 		for _, p := range strings.Split(*port, ",") {
 			cmdArr = append(cmdArr, "-p", p)
 		}
@@ -47,7 +53,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(cmd.String())
+	//fmt.Println(cmd.String())
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
---
+++ version/version.go
@@ -0,0 +1,6 @@
+package version
+
+var (
+	Name = "run-in-linux"
+	Version = "v0.0.4"
+)
